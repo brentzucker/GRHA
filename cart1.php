4fcf5043210c5b4a61458b63df8766df4afe540c
JonSelf <jon.self@hotmail.com>
2015-04-26 17:07:59 -0400

    Added cart functionality
    Cart has been designed, also some small changes have been added to
    various places

--- cart1.php
+++ cart1.php
@@ -8,21 +8,21 @@ session_start();
 open_html("Home");
 
 echo<<<_END
-    <div id="slideshow_container">  
+    <div id="slideshow_container">
 	  <div class="slideshow">
 	    <ul class="slideshow">
           <li class="show"><img width="940" height="250" src="images/main.png" alt="&quot;Georgia Rural Health Association&quot;" /></li>
           <li><img width="940" height="250" src="http://placehold.it/940x250" alt="&quot;Enter your caption here&quot;" /></li>
-        </ul> 
-	  </div> 	
-	</div>	
-    
-	
-	
+        </ul>
+	  </div>
+	</div>
+
+
+
 	<div id="site_content">
 _END;
 
-	showCart();
+addToCart();
 
 echo <<<_END
 	</div>
--- page_functions.php
+++ page_functions.php
@@ -38,9 +38,16 @@ _END;
 
 function close_html()
 {
+	$user;
+	if(isset($_SESSION['Developer']))
+	{
+		$user = $_SESSION['Developer'];
+	}
+	else
+		$user = "";
 echo<<<_END
 	<footer>
-		<p>Copyright &copy; 2015 Georgia Rural Health Association | logged in as: </p><a href="logout.php">Log Out</a>
+		<p>Copyright &copy; 2015 Georgia Rural Health Association | logged in as: $user</p><a href="logout.php">Log Out</a>
     </footer>
 	
   </div>
@@ -60,14 +67,14 @@ function checkLogin()
 		$un = $_POST['userName'];
 		$pw = $_POST['password'];
 	
-	
 		$sql="SELECT * FROM customers WHERE userName='$un' and password='$pw'";
 		$result=mysql_query($sql);
+		if (!$result) die("failed".mysql_error());
 		$count=mysql_num_rows($result);
 		
 		if($count==1)
 		{
-			$_SESSION['Developer'];
+			$_SESSION['Developer'] = $un;
 			header("Location:index.php");
 		}
 	}
@@ -82,11 +89,93 @@ function createAccount()
 		$cust = $_POST['custID'];
 		$fn = $_POST['create_fname'];
 		$ln = $_POST['create_lname'];
-		$ad = $_POST['address'];
+		$ad = $_POST['create_address'];
 		
 		$query = "INSERT INTO customers (firstName, lastName, userName, password, customerID, address) VALUES ('$fn', '$ln','$un', '$pw', '$cust', '$ad')";
 		mysql_query($query);
 	}
+}
+
+function showCart()
+{
+	if (isset($_POST['clear']))
+	{
+		$_SESSION['cart']=array();
+		$_SESSION['totalP']=0;
+		session_destroy();
+		echo "now empty";
+	}
+	else {
+		print_order();
+	}
+}
+function print_order()
+{
+	if (isset($_SESSION['cart']))
+	{
+		echo <<<_END
+		<h2>Your Cart</h2>
+		<table class="cart-table" style="border-collapse: collapse">
+  		<tr>
+			<th>Conference Name</th>
+			<th>Price</th>
+			<th>Quantity</th>
+			<th>Row Total</th>
+  		</tr>
+_END;
+		foreach ($_SESSION['cart'] as $conference => $values)
+		{
+			echo "<tr>";
+			echo "<td>$values[name]</td>";
+			echo "<td>$values[price]</td>";
+			echo "<td>$values[quantity]</td>";
+
+			$subtotal = $values['price']*$values['quantity'];
+			echo "<td> $subtotal </td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+
+		echo "<b>Total price is $".$_SESSION['totalPrice']."</b>";
+
+		echo <<<_END
+		<form name="order" method="post" action="cart.php?clear=1">
+	  		<p>
+    			<input name="clear" type="submit" value="Clear Cart" />
+  			</p>
+		</form>
+_END;
+	}
+	else
+	{
+		echo "Empty cart.";
+	}
+}
+
+function addToCart()
+{
+	$price = $_POST['price'];
+	$quantity = $_POST['quantity'];
+	$name = $_POST['name'];
+	$conferenceID = $_POST['conferenceID'];
+
+	if (!isset($_SESSION['totalPrice']))
+	{
+		$_SESSION['totalPrice']= $quantity*$price;
+	}
+	else
+		$_SESSION['totalPrice']+= $quantity*$price;
+
+	if (!isset($_SESSION['cart'][$conferenceID]))
+	{
+		$_SESSION['cart'][$conferenceID]['price']=$price;
+		$_SESSION['cart'][$conferenceID]['quantity']=$quantity;
+		$_SESSION['cart'][$conferenceID]['name']=$name;
+	}
+	else
+		$_SESSION['cart'][$conferenceID]['quantity']+=$quantity;
+
+	echo "Conferences added to cart, view items in the \"cart\" tab";
 }
 
 function getConferences()
@@ -95,6 +184,7 @@ function getConferences()
 $query = "SELECT conferenceID, name, price, location FROM  conferences";
 
 $result = mysql_query($query);
+if (!$result) die("failed".mysql_error());
 $rows=mysql_num_rows($result);
 
 echo '<table class="conference-table">';
